2fc7b3fd60430b2ea787f7df970f124c76f98d38
Administrator <Administrator@20180131-144644>
2018-02-28 11:30:03 +0800

    sql数据库

---
+++ WebContent/sql/test.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.0.22-community-nt : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `person` */
+
+DROP TABLE IF EXISTS `person`;
+
+CREATE TABLE `person` (
+  `id` int(10) NOT NULL auto_increment,
+  `name` varchar(50) default NULL,
+  `age` varchar(50) default NULL,
+  `sex` varchar(50) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `person` */
+
+insert  into `person`(`id`,`name`,`age`,`sex`) values (1,'黄晓明','35','男'),(2,'杨幂','31','女'),(3,'赵丽颖','31','女');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL auto_increment COMMENT 'id',
+  `user_name` varchar(50) default NULL,
+  `password` varchar(100) default NULL,
+  `add_time` datetime default NULL,
+  `is_used` int(2) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`user_name`,`password`,`add_time`,`is_used`) values (5,'mm','123456','2018-02-06 00:00:00',1),(6,'xx','123456','2018-02-07 00:00:00',1),(7,'tt','123456','2018-01-31 00:00:00',1),(8,'ww','123456','2018-02-01 00:00:00',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
